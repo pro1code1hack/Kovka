6691e5e6bd92818a6c367fc1c16b2351d9cc79c5
hacking <western.ant2@gmail.com>
2021-11-29 17:40:16 +0200

    Initial commit

---
+++ kovka/__init__.py
@@ -0,0 +1,15 @@
+# initialize application structure
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'      # the same what
+
+from kovka import routes
